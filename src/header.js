d7fb2295167708ebe602e6f32fa3c105c15b09d3
Tim <timothy.george.taylor@gmail.com>
2021-12-15 15:28:41 -0700

    added accessibility skip link

--- src/header.js
+++ src/header.js
@@ -4,7 +4,14 @@ export default function () {
 
     const h1 = document.createElement("H1");
     h1.innerText = "todos";
+    const text = document.createTextNode("Skip links");
+    const skip = document.createElement('A');
+    skip.appendChild(text);
+    skip.class = "skip";
+    skip.href = "#content";
+
     header.appendChild(h1);
+    header.appendChild(skip);
 
     return header;
 }
